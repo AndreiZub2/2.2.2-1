7cb082f24b3581273b6ca4d52b7dbf2a2f544b02
AndreeiZub2 <andrei_zubkovr@mail.ru>
2023-09-03 23:54:43 +0400

    qwe

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,14 +1,33 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CarService {
 
-    public static List<Car> carsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
+@Component
+public class CarService implements Cars {
+    private List<Car> list  = new ArrayList<>();
+
+    public List<Car> carList (){
+
+        list.add(new Car("VAZ", "RED", 2000));
+        list.add(new Car("GAZ", "YELLOW", 1995));
+        list.add(new Car("UAZ", "BLACK", 2010));
+        list.add(new Car("KAMAZ", "GREEN", 2020));
+        list.add(new Car("LUAZ", "BLUE", 1985));
+        return list;
+    }
+
+    @Override
+    public List<Car> carsCount(List<Car> list, int number) {
+        if (number == 0 || number > 5)
+           return list;
         return list.stream().limit(number).collect(Collectors.toList());
     }
+
+
 }
